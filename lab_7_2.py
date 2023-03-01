dcd430546e3c50b4bbe55daba1056bec4509b5aa
Nikita39Alt <126085446+Nikita39Alt@users.noreply.github.com>
2023-03-01 18:35:09 +0200

    Update lab_7_2.py

--- lab_7_2.py
+++ lab_7_2.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as mpl
 import matplotlib.animation as animator
 
-colors = ['r','orange','yellow','g','cyan','b','purple']
-
 fig,ax = mpl.subplots()
 circle, = mpl.plot([],[],'o')
 def circle_extend(t):
-    x, y = t*np.sin(np.arange(0,2*np.pi,0.1)), t*np.cos(np.arange(0,2*np.pi,0.1))
+    x, y = t*np.sin(np.arange(0,2*np.pi+0.1,0.1)), t*np.cos(np.arange(0,2*np.pi+0.1,0.1))
     return [x,y]
 
 def animate(i):
-    circle = mpl.plot(circle_extend(i)[0],circle_extend(i)[1],color=colors[i%7])
+    circle.set_data(circle_extend(i))
 
 mpl.axis('equal')
+ax.set_xlim(-10,10)
+ax.set_ylim(-10,10)
 
-ani = animator.FuncAnimation(fig, animate, frames = 50, interval = 100)
-ani.save('lab_7_2_a.gif')
+ani = animator.FuncAnimation(fig, animate, frames = 100, interval = 30)
+ani.save('lab_7_2.gif')
